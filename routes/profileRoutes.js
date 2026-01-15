d623f694550b10630ab9b49dff7ebb966f708b13
Shari Dev <freelancingwithsharishah@gmail.com>
2026-01-15 10:40:34 +0500

    commet remove

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -11,19 +11,10 @@ import upload from "../middleware/upload.js"
 
 const router = express.Router()
 
-// CREATE a new profile
 router.post("/", protect, upload.single("image"), createProfile)
-
-// GET the logged-in user's profile
 router.get("/me", protect, getMyProfile)
-
-// UPDATE logged-in user's profile
 router.put("/me", protect, upload.single("image"), updateProfile)
-
-// GET all approved profiles (public)
 router.get("/approved", getApprovedProfiles)
-
-// ADMIN: update profile status
 router.put("/:id/status", protect, adminOnly, updateProfileStatus)
 
 export default router
