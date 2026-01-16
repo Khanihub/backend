0e91d874c1d95f60c00db5b20e358b9b17b5fa47
Shari Dev <freelancingwithsharishah@gmail.com>
2026-01-16 15:43:19 +0500

    updated

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -11,23 +11,11 @@ import { protect, adminOnly } from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
-
-// Create new profile
 router.post("/", protect, upload.single("image"), createProfile);
-
-// Get my profile
 router.get("/me", protect, getMyProfile);
-
-// Update my profile
 router.put("/me", protect, upload.single("image"), updateProfile);
-
-// Delete profile
 router.delete("/delete", protect, deleteProfile);
-
-// Get approved profiles
 router.get("/approved", getApprovedProfiles);
-
-// Admin: approve/reject profiles
 router.put("/:id/status", protect, adminOnly, updateProfileStatus);
 
 export default router;
